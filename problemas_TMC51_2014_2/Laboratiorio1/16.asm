13e00eb899e7b539dd67018b3f708f37b713bfd8
Juan Carlos Tuesta <jctuesta@uni.pe>
2017-04-03 07:57:06 -0700

    Create 16.asm

---
+++ problemas_TMC51_2014_2/Laboratiorio1/16.asm
@@ -0,0 +1,14 @@
+org 8000h
+sdelay equ 142h
+mov P1,#0h
+repite:
+	mov P1,#255 ;prende todos los leds
+	lcall sdelay
+	lcall sdelay
+	mov P1,#0h
+	lcall sdelay
+	lcall sdelay
+	mov P1,#255
+	
+	sjmp repite
+	END

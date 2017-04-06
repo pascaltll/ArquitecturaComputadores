ad0ace1533d74292129f2c6951bbca7b58b588e5
Juan Carlos Tuesta <jctuesta@uni.pe>
2017-04-06 13:48:52 -0700

    Update juegodeluces.asm

--- problemas_TMC51_2014_2/Laboratiorio1/juegodeluces.asm
+++ problemas_TMC51_2014_2/Laboratiorio1/juegodeluces.asm
@@ -1,7 +1,7 @@
 org 8000h;una barra de progreso
 sdelay equ 142h
 repite:
-	mov R3,#8
+	mov R3,#15
 	mov R2,#0
 lazo:
 	mov A,R2
@@ -15,9 +15,7 @@ codigo:
 	inc A
 	movc A,@A+PC
 	ret
-	db 1,2,,4,8,16,32,64
+	db 1,2,4,8,16,32,64,128,64,32,16,8,4,2,1
 	
 END
-
-    Contact GitHub API Training Shop Blog About 
 
